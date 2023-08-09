90b21c767bb652d2e1b5f39daaa527aec5bae4c1
Dmitry <103439106+Solmyrik@users.noreply.github.com>
2023-08-09 09:34:53 +0300

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,54 @@
+<<<<<<< HEAD
+new Swiper('.slider__block-1', {
+  slidesPerView: 3,
+  simulateTouch: true,
+  navigation: {
+    nextEl: '.slider-next-1',
+    prevEl: '.slider-prew-1',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+  //   spaceBetween: 30,
+});
+new Swiper('.slider__block-2', {
+  slidesPerView: 3,
+  simulateTouch: true,
+  navigation: {
+    nextEl: '.slider-next-2',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+  //   spaceBetween: 30,
+});
+new Swiper('.slider__block-3', {
+  slidesPerView: 3,
+  simulateTouch: true,
+  navigation: {
+    nextEl: '.slider-next-3',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+  //   spaceBetween: 30,
+});
+=======
 new Swiper('.slider__block-1', {
   slidesPerView: 3,
   simulateTouch: true,
@@ -46,3 +97,4 @@ new Swiper('.slider__block-3', {
   },
   //   spaceBetween: 30,
 });
+>>>>>>> 26719688ced112d6f4dc95f7d6f0a7e742183ed6
